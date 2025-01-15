1cedacbfcbb241fe2105fa0aca353440168a7e94
Aiden Johnson <65328952+tgmstudios@users.noreply.github.com>
2025-01-14 22:11:40 -0500

    rid of desktop

--- inventory_script.py
+++ inventory_script.py
@@ -26,7 +26,7 @@ def generate_inventory(env_count):
         inventory["all"]["children"]["debian"]["hosts"][f"debian-10-internal-{i}"] = {"ansible_host": f"10.{i}.1.11", "ansible_user": "debian"}
         inventory["all"]["children"]["ubuntu"]["hosts"][f"ubuntu-18-user-{i}"] = {"ansible_host": f"10.{i}.2.10", "ansible_user": "ubuntu"}
         inventory["all"]["children"]["ubuntu"]["hosts"][f"ubuntu-20-desktop-user-{i}"] = {"ansible_host": f"10.{i}.2.12", "ansible_user": "ubuntu"}
-        inventory["all"]["children"]["splunk"]["hosts"][f"splunk-public-{i}"] = {"ansible_host": f"10.{i}.3.10", "ansible_user": "splunk"}
+        inventory["all"]["children"]["splunk"]["hosts"][f"splunk-public-{i}"] = {"ansible_host": f"10.{i}.3.10", "ansible_user": "debian"}
         inventory["all"]["children"]["centos"]["hosts"][f"centos-7-public-{i}"] = {"ansible_host": f"10.{i}.3.11", "ansible_user": "centos"}
         inventory["all"]["children"]["fedora"]["hosts"][f"fedora-31-public-{i}"] = {"ansible_host": f"10.{i}.3.12", "ansible_user": "fedora"}
 
